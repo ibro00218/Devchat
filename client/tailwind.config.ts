4aab21992fd42a3d5646e6e148a7056b56502d45
Vansh <ibro00218@gmail.com>
2025-05-23 16:34:54 +0530

    Updated client

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", "sans-serif"]
+        sans: ["var(--font-sans)", "sans-serif"],
       },
       borderRadius: {
         lg: "var(--radius)",
